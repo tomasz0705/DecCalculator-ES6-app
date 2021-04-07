87f4bb080c91fad1599b158a1a28e419d9e379be
Tomasz Stolarczyk <73778092+tomasz0705@users.noreply.github.com>
2021-04-07 17:07:15 +0200

    Import klasy DecCalculator i dodanie instancji klasy do EventListener'a w pliku app.js

---
+++ DecCalculator_app/js/app.js
@@ -0,0 +1,7 @@
+import BinaryCalculator from "./BinaryCalculator";
+import DecCalculator from "./DecCalculator";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const bitCalc = new BinaryCalculator(".binary-calculator");
+  const decCalc = new DecCalculator(".dec-calculator");
+});
